5f53d9de3ce38b169797aff84040db721567de94
박성현 <qkr970504@gmail.com>
2025-03-16 22:28:05 +0900

    [init] build project

---
+++ movie-infrastructure/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    implementation(project(":movie-business"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "movie-multi-module"
+
+include("movie-api")
+include("movie-business")
+include("movie-infrastructure")
+include("movie-application")
