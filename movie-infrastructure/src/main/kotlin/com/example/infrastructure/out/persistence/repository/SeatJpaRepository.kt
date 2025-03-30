81c03ed34a416573ed085336ea21cabfddd4db2f
박성현 <qkr970504@gmail.com>
2025-03-31 02:08:06 +0900

    [Lock 적용] 분산락 + 낙관적 락

--- movie-infrastructure/src/main/kotlin/com/example/infrastructure/out/persistence/repository/SeatJpaRepository.kt
+++ movie-infrastructure/src/main/kotlin/com/example/infrastructure/out/persistence/repository/SeatJpaRepository.kt
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query
 
 interface SeatJpaRepository: JpaRepository<SeatEntity, Long> {
 
-    //@Lock(LockModeType.OPTIMISTIC)
+    @Lock(LockModeType.OPTIMISTIC)
     @Query("select s from SeatEntity s where s.id in :seatIds")
     fun findBySeatIdsWithLock(seatIds: List<Long>): List<SeatEntity>
 
